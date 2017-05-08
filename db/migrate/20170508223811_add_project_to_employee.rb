c3bd366c4014fa3efb05ff3e3bf741bb31231db1
Ilene Gorski and Max Reichman <ilene+max@>
2017-05-08 15:40:49 -0700

    add projects table

---
+++ db/migrate/20170508223811_add_project_to_employee.rb
@@ -0,0 +1,5 @@
+class AddProjectToEmployee < ActiveRecord::Migration[5.1]
+  def change
+    add_column(:employees, :project_id, :integer)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508201146) do
+ActiveRecord::Schema.define(version: 20170508223811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20170508201146) do
   create_table "employees", force: :cascade do |t|
     t.string "name"
     t.integer "division_id"
+    t.integer "project_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
   end
 
 end
